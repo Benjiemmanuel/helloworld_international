a0259c04855c297b164b239163bb2d73412b6d20
Benjiemmanuel <101885631+Benjiemmanuel@users.noreply.github.com>
2023-11-08 11:56:25 +0100

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,4 @@ alert("Hello World");
 console.log("Hello World");
 console.log("Hello World Vadym Voytas Hello World from local");
 console.log("Hello World! I'm Usama Rao from Pakistan");
+console.log("Hello world! I'm Benjamin Emmanuel from Nigeria");
